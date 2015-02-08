42f515d4f79c1c9a2e736afd1f6e05f6e2a84c71
Yun William Yu <ywy@mit.edu>
2015-02-08 07:08:55 -0500

    Slightly more robust wordcounter

--- wordcount.sh
+++ wordcount.sh
@@ -5,7 +5,7 @@
 file="main.pdf"
 
 pdftotext ${file} ${file}.txt
-refline=`grep -P "^References$" ${file}.txt -n | head -n 1 | cut -sd: -f1`
+refline=`grep -P "References$" ${file}.txt -n | head -n 1 | cut -sd: -f1`
 sed -n "1,$((${refline}-1))p" ${file}.txt > ${file}.txt.body
 sed -n "${refline},\$p" ${file}.txt > ${file}.txt.ref
 wc ${file}.txt ${file}.txt.body ${file}.txt.ref
